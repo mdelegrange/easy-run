daa970e87d863e312b86482407fbbc6c534259c1
matthieu delegrange <mdelegrange@gmail.com>
2018-12-03 18:13:04 +0100

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Objective creation
+  resources :marathons, only[:index] do
+    resources :objectives, only: [:create]
+  end
+  resources :races, only: [:index] do
+    resources :runs, only: [:create]
+  end
+
+  # Dashboard
+  resources :objectives, only: [:index, :show] do
+    resources :runs, only [:index] do
+  end
+  resources :runs, only [:show, :edit, :update, :destroy] do
+    member do
+      patch :register
+    end
+  end
+
+  resource :training, only: [:show] do
+    resource :sessions, only: [:show], module: :trainings
+  end
+
+  resource :profile, only: [:show, :edit, :update]
 end
