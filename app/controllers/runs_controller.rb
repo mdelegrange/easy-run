06cc216fa49c9727f0b0e95d17f69fb1add09bf1
yassine jay <yassinejay@hotmail.fr>
2018-12-04 18:59:10 +0100

    WIP Added Update Action To Run Controller

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,16 +1,16 @@
 class RunsController < ApplicationController
 
   def create
-  	@run = Run.new(run_params)
-  	@race = Race.find(params[:race_id])
-  	@run.race = @race
-  	@run.objective = current_user.pending_objective
-  	@run.status = 'pending'
-  	if @run.save
-  	  race_runs_path(@race)
-  	else
-  	  render :new
-  	end
+    @run = Run.new(run_params)
+    @race = Race.find(params[:race_id])
+    @run.race = @race
+    @run.objective = current_user.pending_objective
+    @run.status = 'pending'
+    if @run.save
+      race_runs_path(@race)
+    else
+      render :new
+    end
   end
 
   def update
@@ -22,6 +22,6 @@ class RunsController < ApplicationController
   private
 
   def run_params
-  	params.require(:run).permit(:targeted_time)
+    params.require(:run).permit(:targeted_time)
   end
 end
