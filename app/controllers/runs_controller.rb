b9e4ce482c689eac8fc240c94537b2f7ec55eabf
yassine jay <yassinejay@hotmail.fr>
2018-12-04 17:50:17 +0100

    created run controller with create action(runs#create)

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,9 +1,22 @@
 class RunsController < ApplicationController
 
   def create
-  	@run = Run.new(status: 'pending')
+  	@run = Run.new(run_params)
   	@race = Race.find(params[:race_id])
   	@run.race = @race
-  	@run.objective = @race.objective
+  	@run.objective = current_user.pending_objective
+  	@run.status = 'pending'
+  	if @run.save
+  		race_runs_path(@race)
+  	else
+  	  render :new
+  	end
+
+  end
+
+  private
+
+  def run_params
+  	params.require(:run).permit(:targeted_time)
   end
 end
