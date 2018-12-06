41f39b2254d5ae6e8fdc4f4f1d58c619b2621840
valentin burgaud <burval3@gmail.com>
2018-12-06 13:25:09 +0100

    add level to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_111343) do
+ActiveRecord::Schema.define(version: 2018_12_06_122340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2018_12_06_111343) do
     t.string "first_name"
     t.string "last_name"
     t.string "photo"
+    t.integer "level"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
