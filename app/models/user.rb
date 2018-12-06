1772e4c0bcc6a4e6a02a42c8b25cb9f89114fc9e
Pierre Hamon <pierrehamon@gmail.com>
2018-12-06 13:06:50 +0100

    profiles edit and update done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+  mount_uploader :photo, PhotoUploader
+
   has_one :current_objective, -> { where(status: 'current') }, class_name: 'Objective'
   has_one :current_training, -> { where(status: 'current') }, class_name: 'Training'
   has_many :objectives
