90cf09852d5378a29d80c7dff912101673ac407e
matthieu delegrange <mdelegrange@gmail.com>
2018-12-07 11:43:40 +0100

    updating quiz and front with workflow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_165447) do
+ActiveRecord::Schema.define(version: 2018_12_07_104106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,8 +95,10 @@ ActiveRecord::Schema.define(version: 2018_12_06_165447) do
     t.string "first_name"
     t.string "last_name"
     t.string "photo"
-    t.string "level"
+    t.integer "level"
     t.integer "targeted_distance"
+    t.string "has_already_run"
+    t.string "department"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
