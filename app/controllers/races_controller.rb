280eff5abaff0ea27133e3d09f94aa362e13251b
matthieu delegrange <mdelegrange@gmail.com>
2018-12-08 09:30:24 +0100

    Display races choices

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,6 +1,6 @@
 class RacesController < ApplicationController
-
   skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @user = current_user
     @departments_options = Race::DEPARTMENTS.map { |label, value| [label, value] }
@@ -8,5 +8,20 @@ class RacesController < ApplicationController
   end
 
   def show
+    @race = Race.find(params[:id])
+
+    @objective = Objective.create(
+      user: current_user,
+      race_id: params[@race],
+      kind: "marathon",
+      distance: 42195,
+      status: "current"
+      )
+
+    @departments_options = Race::DEPARTMENTS.map { |label, value| [label, value] }
+    @month = @race.date.month
+    @races = Race.all
+
+
   end
 end
