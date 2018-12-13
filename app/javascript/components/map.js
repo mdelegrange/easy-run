01d8db8303d88599e62e1663d22c111640b7288d
matthieu delegrange <mdelegrange@gmail.com>
2018-12-13 11:50:15 +0100

    improve map

---
+++ app/javascript/components/map.js
@@ -0,0 +1,154 @@
+// const ara = document.getElementById("ara");
+// const araImage = document.getElementById("ara-hover")
+
+// ara.addEventListener('mouseenter', (event) => {
+//   araImage.classList.remove("hidden");
+// })
+
+// araImage.addEventListener('mouseleave', (event) => {
+//   araImage.classList.add("hidden");
+// })
+
+// const bfc = document.getElementById("bfc");
+// const bfcImage = document.getElementById("bfc-hover")
+
+// bfc.addEventListener('mouseenter', (event) => {
+//   bfcImage.classList.remove("hidden");
+// })
+
+// bfcImage.addEventListener('mouseleave', (event) => {
+//   bfcImage.classList.add("hidden");
+// })
+
+
+// //b-hover
+// const b = document.getElementById("b");
+// const bImage = document.getElementById("b-hover")
+
+// b.addEventListener('mouseenter', (event) => {
+//   bImage.classList.remove("hidden");
+// })
+
+// bImage.addEventListener('mouseleave', (event) => {
+//   bImage.classList.add("hidden");
+// })
+
+// //cvdl-hover
+// const cvdl = document.getElementById("cvdl");
+// const cvdlImage = document.getElementById("cvdl-hover")
+
+// cvdl.addEventListener('mouseenter', (event) => {
+//   cvdlImage.classList.remove("hidden");
+// })
+
+// cvdlImage.addEventListener('mouseleave', (event) => {
+//   cvdlImage.classList.add("hidden");
+// })
+
+// //c-hover
+// const c = document.getElementById("c");
+// const cImage = document.getElementById("c-hover")
+
+// c.addEventListener('mouseenter', (event) => {
+//   cImage.classList.remove("hidden");
+// })
+
+// cImage.addEventListener('mouseleave', (event) => {
+//   cImage.classList.add("hidden");
+// })
+
+// //ge-hover
+// const ge = document.getElementById("ge");
+// const geImage = document.getElementById("ge-hover")
+
+// ge.addEventListener('mouseenter', (event) => {
+//   geImage.classList.remove("hidden");
+// })
+
+// geImage.addEventListener('mouseleave', (event) => {
+//   geImage.classList.add("hidden");
+// })
+
+// //hdf-hover
+// const hdf = document.getElementById("hdf");
+// const hdfImage = document.getElementById("hdf-hover")
+
+// hdf.addEventListener('mouseenter', (event) => {
+//   hdfImage.classList.remove("hidden");
+// })
+
+// hdfImage.addEventListener('mouseleave', (event) => {
+//   hdfImage.classList.add("hidden");
+// })
+
+// //idf-hover
+// const idf = document.getElementById("idf");
+// const idfImage = document.getElementById("idf-hover")
+
+// idf.addEventListener('mouseenter', (event) => {
+//   idfImage.classList.remove("hidden");
+// })
+
+// idfImage.addEventListener('mouseleave', (event) => {
+//   idfImage.classList.add("hidden");
+// })
+
+// //n-hover
+// const n = document.getElementById("n");
+// const nImage = document.getElementById("n-hover")
+
+// n.addEventListener('mouseenter', (event) => {
+//   nImage.classList.remove("hidden");
+// })
+
+// nImage.addEventListener('mouseleave', (event) => {
+//   nImage.classList.add("hidden");
+// })
+
+// //na-hover
+// const na = document.getElementById("na");
+// const naImage = document.getElementById("na-hover")
+
+// na.addEventListener('mouseenter', (event) => {
+//   naImage.classList.remove("hidden");
+// })
+
+// naImage.addEventListener('mouseleave', (event) => {
+//   naImage.classList.add("hidden");
+// })
+
+// //o-hover
+// const o = document.getElementById("o");
+// const oImage = document.getElementById("o-hover")
+
+// o.addEventListener('mouseenter', (event) => {
+//   oImage.classList.remove("hidden");
+// })
+
+// oImage.addEventListener('mouseleave', (event) => {
+//   oImage.classList.add("hidden");
+// })
+
+// //pdll-hover
+// const pdll = document.getElementById("pdll");
+// const pdllImage = document.getElementById("pdll-hover")
+
+// pdll.addEventListener('mouseenter', (event) => {
+//   pdllImage.classList.remove("hidden");
+// })
+
+// pdllImage.addEventListener('mouseleave', (event) => {
+//   pdllImage.classList.add("hidden");
+// })
+
+// //paca-hover
+// const paca = document.getElementById("paca");
+// const pacaImage = document.getElementById("paca-hover")
+
+// paca.addEventListener('mouseenter', (event) => {
+//   pacaImage.classList.remove("hidden");
+// })
+
+// pacaImage.addEventListener('mouseleave', (event) => {
+//   pacaImage.classList.add("hidden");
+// })
